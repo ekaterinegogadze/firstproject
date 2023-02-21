3db3ac5e995a9a2a1c08d231a76ec89195a18598
ekaterinegogadze <eka.gogadze88@gmail.com>
2023-02-22 00:20:42 +0400

    add homework

---
+++ homework/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // დავალება 1
+
+
+       String name = "eka";
+       int in = 105;
+       float ft = 12.3f;
+       char cr = '!';
+       boolean bl = in > ft;
+       System.out.println(name);
+       System.out.println(in);
+       System.out.println(ft);
+       System.out.println(cr);
+       System.out.println(bl);
+
+
+        // დავალება 2
+
+        int result =5-3;
+        int result1 =10/2;
+        System.out.println(result);
+        System.out.println(result1);
+
+        // დავალება 3
+
+        int a = 7;
+        int b = 9;
+        boolean log = a > 5 || b == 9;
+        System.out.println(log);
+
+        // დავალება 4
+
+        int d = 2023;
+        int w = 1988;
+        int g =d-w;
+        System.out.println("ჩემი დაბადების წელია" +" "+ w);
+        System.out.println("ჩემი ასაკია"+" "+g);
+
+
+
+
+    }
+
+}
